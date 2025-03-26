2d50eb92ea09c4a03271d94f240be34ef50c29aa
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-03-26 11:57:58 +0100

    Resolve error from AnnotationCreation.test.js

--- __tests__/AnnotationForm.test.js
+++ __tests__/AnnotationForm.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
-import AnnotationCreation from '../src/AnnotationCreation';
-import AnnotationDrawing from '../src/AnnotationDrawing';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import AnnotationForm from '../src/annotationForm/AnnotationForm';
+import AnnotationDrawing from '../src/annotationForm/AnnotationFormOverlay/AnnotationDrawing';
 import TextEditor from '../src/TextEditor';
-import ImageFormField from '../src/ImageFormField';
+import ImageFormField from '../src/annotationForm/AnnotationFormOverlay/ImageFormField';
 
 /** */
 function createWrapper(props) {
   return shallow(
-    <AnnotationCreation
+    <AnnotationForm
       id="x"
       config={{ annotation: {} }}
       receiveAnnotation={jest.fn()}
@@ -23,23 +23,33 @@ describe('AnnotationCreation', () => {
   let wrapper;
   it('renders a form', () => {
     wrapper = createWrapper();
-    expect(wrapper.dive().find('form').length).toBe(1);
+    expect(wrapper.dive()
+      .find('form').length)
+      .toBe(1);
   });
   it('form has button toggles', () => {
     wrapper = createWrapper();
-    expect(wrapper.dive().find(ToggleButtonGroup).length).toBe(3);
+    expect(wrapper.dive()
+      .find(ToggleButtonGroup).length)
+      .toBe(3);
   });
   it('adds the AnnotationDrawing component', () => {
     wrapper = createWrapper();
-    expect(wrapper.dive().find(AnnotationDrawing).length).toBe(1);
+    expect(wrapper.dive()
+      .find(AnnotationDrawing).length)
+      .toBe(1);
   });
   it('adds the TextEditor component', () => {
     wrapper = createWrapper();
-    expect(wrapper.dive().find(TextEditor).length).toBe(1);
+    expect(wrapper.dive()
+      .find(TextEditor).length)
+      .toBe(1);
   });
   it('adds the ImageFormField component', () => {
     wrapper = createWrapper();
-    expect(wrapper.dive().find(ImageFormField).length).toBe(1);
+    expect(wrapper.dive()
+      .find(ImageFormField).length)
+      .toBe(1);
   });
   it('can handle annotations without target selector', () => {
     wrapper = createWrapper({
