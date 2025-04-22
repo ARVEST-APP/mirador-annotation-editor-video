aa9f1ccfea9485f30b497a846d1c5343e37dbd28
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-04-22 10:58:42 +0200

    Use multiple body in network template

--- src/annotationForm/AnnotationFormBody.js
+++ src/annotationForm/AnnotationFormBody.js
@@ -79,6 +79,8 @@ export default function AnnotationFormBody(
               saveAnnotation={saveAnnotation}
               t={t}
               windowId={windowId}
+              commentTemplate={config?.annotation?.commentTemplates ?? []}
+              tagsSuggestions={config?.annotation?.tagsSuggestions ?? []}
             />
           )
         }
--- src/annotationForm/NetworkCommentTemplate.js
+++ src/annotationForm/NetworkCommentTemplate.js
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import uuid from 'draft-js/lib/uuid';
 import { Grid } from '@mui/material';
-import TextFormSection from './TextFormSection';
 import TargetFormSection from './TargetFormSection';
 import ManifestNetworkFormSection from './ManifestNetworkFormSection';
 import { TEMPLATE } from './AnnotationFormUtils';
 import AnnotationFormFooter from './AnnotationFormFooter';
 import { resizeKonvaStage } from './AnnotationFormOverlay/KonvaDrawing/KonvaUtils';
+import { TextCommentInput } from './TextCommentInput';
+import { MultiTagsInput } from './MultiTagsInput';
 
 /** Form part for edit annotation content and body */
 function NetworkCommentTemplate(
   {
     annotation,
     closeFormCompanionWindow,
+    commentTemplate,
     playerReferences,
     saveAnnotation,
     t,
+    tagsSuggestions,
     windowId,
   },
 ) {
@@ -31,17 +34,43 @@ function NetworkCommentTemplate(
         value: '',
       },
       maeData: {
+        tags: [],
         manifestNetwork: '',
         target: null,
         templateType: TEMPLATE.MANIFEST_TYPE,
+        textBody: {
+          purpose: 'describing',
+          type: 'TextualBody',
+          value: '',
+        },
       },
       motivation: 'commenting',
       target: null,
     };
-  } else if (maeAnnotation.maeData.target.drawingState && typeof maeAnnotation.maeData.target.drawingState === 'string') {
-    maeAnnotation.maeData.target.drawingState = JSON.parse(
-      maeAnnotation.maeData.target.drawingState,
-    );
+  } else {
+    if (maeAnnotation.maeData.target.drawingState
+      && typeof maeAnnotation.maeData.target.drawingState === 'string') {
+      maeAnnotation.maeData.target.drawingState = JSON.parse(
+        maeAnnotation.maeData.target.drawingState,
+      );
+    }
+    // We support only one textual body
+    if (maeAnnotation.body && maeAnnotation.body.length > 0) {
+      maeAnnotation.maeData.textBody = maeAnnotation.body.find((body) => body.purpose === 'describing');
+      maeAnnotation.maeData.tags = maeAnnotation.body.filter((body) => body.purpose === 'tagging')
+        .map((tag) => ({
+          label: tag.value,
+          value: tag.value,
+        }));
+    } else {
+      // Only for retro compatibility in Arvest
+      maeAnnotation.maeData.textBody = {
+        purpose: 'describing',
+        type: 'TextualBody',
+        value: '',
+      };
+      maeAnnotation.maeData.tags = [];
+    }
   }
 
   const [annotationState, setAnnotationState] = useState(maeAnnotation);
@@ -60,12 +89,53 @@ function NetworkCommentTemplate(
   /**
    * Update the annotation's Body
    * */
-  const updateAnnotationTextBody = (newTextValue) => {
-    const newBody = annotationState.body;
-    newBody.value = newTextValue;
+  const updateAnnotationTextualBodyValue = (newTextValue) => {
     setAnnotationState({
       ...annotationState,
-      body: newBody,
+      maeData: {
+        ...annotationState.maeData,
+        textBody: {
+          ...annotationState.maeData.textBody,
+          value: newTextValue,
+        },
+      },
+    });
+  };
+
+  /**
+   * When the user selects a template, we change text comment and try to add the tag with same name
+   * @param selectedTemplate
+   */
+  const onChangeTemplate = (selectedTemplate) => {
+    const associatedTag = mappedSuggestionsTags.find((tag) => tag.value === selectedTemplate.label);
+    if (associatedTag) {
+      if (!annotationState.maeData.tags.find((tag) => tag.value === associatedTag.value)) {
+        setAnnotationState({
+          ...annotationState,
+          maeData: {
+            ...annotationState.maeData,
+            tags: [...annotationState.maeData.tags, associatedTag],
+            textBody: {
+              ...annotationState.maeData.textBody,
+              value: selectedTemplate.value,
+            },
+          },
+        });
+        return;
+      }
+    }
+
+    updateAnnotationTextualBodyValue(selectedTemplate.value);
+  };
+
+  /** Update annotation with Tag Value * */
+  const setTags = (newTags) => {
+    setAnnotationState({
+      ...annotationState,
+      maeData: {
+        ...annotationState.maeData,
+        tags: newTags,
+      },
     });
   };
 
@@ -79,6 +149,11 @@ function NetworkCommentTemplate(
     });
   };
 
+  /**
+   * Get the base annotation ID
+   * @param id
+   * @returns {*|null}
+   */
   function getBaseAnnotation(id) {
     if (!id) {
       return null;
@@ -108,6 +183,11 @@ function NetworkCommentTemplate(
     saveAnnotation(annotationState);
   };
 
+  const mappedSuggestionsTags = tagsSuggestions.map((suggestion) => ({
+    label: suggestion,
+    value: suggestion,
+  }));
+
   return (
     <Grid container direction="column" spacing={2}>
       <Grid item>
@@ -118,10 +198,20 @@ function NetworkCommentTemplate(
         />
       </Grid>
       <Grid item>
-        <TextFormSection
-          annoHtml={annotationState.body.value}
-          updateAnnotationBody={updateAnnotationTextBody}
+        <TextCommentInput
+          commentTemplates={commentTemplate}
+          comment={annotationState.maeData.textBody.value}
+          setComment={updateAnnotationTextualBodyValue}
+          onChangeTemplate={onChangeTemplate}
           t={t}
+        />
+      </Grid>
+      <Grid item>
+        <MultiTagsInput
+          t={t}
+          tags={annotationState.maeData.tags}
+          setTags={setTags}
+          tagsSuggestions={mappedSuggestionsTags}
         />
       </Grid>
       <TargetFormSection
